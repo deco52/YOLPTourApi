2d48cc2060e44cbe1a1d552a6033cb6eb09dc7c3
deco52 <deco52.s.k@gmail.com>
2018-12-14 17:22:46 +0900

    20181214 【内容】 ・ツアー機能の不具合の修正(原因：IDの指定間違い) ・ツアー機能に画像表示機能を追加 ・コントロール部分のレイアウト作成(CSSが入れ子でカオス) ・レイアウトの微妙な修正 【今後の予定】 ・ツアー内容(json)の追加・修正 ・メニュー機能を実装したりしなかったりしろ

--- iida_city_tour.js
+++ iida_city_tour.js
@@ -26,13 +26,12 @@ function initialize() {
 	obj.send(null);
 
 	// 各領域にボタンなどをセット
-	document.getElementById("reset").innerHTML = '<input type="button" value="リセット" onclick="Reset()"/>';
-	document.getElementById("tour").innerHTML =
-		'<input type="text" id="tourR" value="5" size="1">周'
-		+ '<input type="button" value="ツアー開始" onclick="StartTour()"/>'
-		+ '<input type="button" value="ツアー停止" onclick="StopTour()"/>';
-	
-		document.getElementById("info").innerHTML = "ここに詳しい情報が表示されます";
+	document.getElementById("round-area").innerHTML = '周回数:<input type="text" id="round-input" class="input-design" value="5" size="1">周';
+	document.getElementById("timer-area").innerHTML = '表示時間：<input type="text" id=timer-input class="input-design" value="2000" size="4">ミリ秒';
+	document.getElementById("tour").innerHTML = '<div class="button-area"><input type="button" class="button-design" value="ツアー開始" onclick="StartTour()"/></div>'
+		+ '<div class="button-area"><input type="button" class="button-design" value="ツアー停止" onclick="StopTour()"/></div>'
+		+  '<div class="button-area"><input type="button"  class="button-design" value="リセット" onclick="Reset()"/></div>';;
+	document.getElementById("info").innerHTML = "ここに詳しい情報が表示されます";
 }
 
 /**
@@ -46,7 +45,7 @@ function ReadJson(text) {
 	for (var i = 0; i < cityData.city.length; i++) {
 		var point = new Y.LatLng(cityData.city[i].lat, cityData.city[i].lng);
 		AddMarker(point, cityData.city[i].name, cityData.city[i].url);
-		AddLink(cityData.city[i].name, point);
+		AddLink(cityData.city[i].name, point, cityData.city[i].url);
 		points[i] = point;
 	}
 	points[i] = points[0];
@@ -75,14 +74,14 @@ function AddMarker(point, name, url) {
  * @param name 
  * @param point 
  */
-function AddLink(name, point) {
+function AddLink(name, point, url) {
 	var lnk = document.createElement("a");
 	lnk.innerHTML = name;
 	lnk.href = "#";
 	lnk.onclick = function () {// 左のリンクがクリックされた場合
 		GoTo(point.lat(), point.lng());
-		document.getElementById("info").innerHTML = name;
-		document.getElementById("img").innerHTML = "<img src=img/" + name +".jpg height=250 width=100% alt=サンプル画像>";
+		document.getElementById("info").innerHTML = name + "<br><a href=" + url + " target=blank>(" + url + ")</a>";
+		document.getElementById("img").innerHTML = "<img src=img/" + name + ".jpg height=250 width=100% alt=" + name + ">";
 	};
 	document.getElementById("link").appendChild(lnk);
 
@@ -102,7 +101,6 @@ function GoTo(lat, lng) { map.setCenter(new Y.LatLng(lat, lng)); }
  */
 function Reset() {
 	document.getElementById("link").innerHTML = "";
-	document.getElementById("reset").innerHTML = "";
 	document.getElementById("tour").innerHTML = "";
 	document.getElementById("info").innerHTML = "";
 	document.getElementById("img").innerHTML = "";
@@ -113,6 +111,7 @@ function Reset() {
 * ↓↓ツアー用の記述↓↓
 */
 var tourMax;// 周回数
+var timerMax;//　1回の表示時間
 var timerID = new Array();
 
 /**
@@ -122,36 +121,43 @@ function StartTour() {
 	var num = 0;
 	var round = 1;
 
-	tourMax = document.getElementById("tour").value * cityData.city.length + 1;
+	tourMax = document.getElementById("round-input").value * cityData.city.length + 1;
+	timerMax =  document.getElementById("timer-input").value * 1;
+	console.log(tourMax);
+	console.log(timerMax);
 	for (var i = 0; i < tourMax; i++) {
 		if (num >= cityData.city.length) {
 			num = 0;
 			round++;
 		}
 		var point = new Y.LatLng(cityData.city[num].lat, cityData.city[num].lng);
-		moveTimer(i, round, point, cityData.city[num].name);
+		moveTimer(i, round, point, cityData.city[num].name, timerMax, cityData.city[num].url);
 		num++;
 	}
 
 	timerID[i] =
-		setTimeout(function () { document.getElementById("now").innerHTML = ""; }, i * 1000);
+		setTimeout(function () { document.getElementById("now").innerHTML = ""; }, i * timerMax);
 }
 
 /**
  * ツアーのタイマー
- * @param {*} i 
- * @param {*} round 
- * @param {*} point 
- * @param {*} name 
+ * @param i 
+ * @param round 周回数の設定(デフォルトは5周)
+ * @param point 
+ * @param name 
+ * @param time 表示時間の設定(デフォルトは1000ミリ秒)
+ * @param url
  */
-function moveTimer(i, round, point, name) {
+function moveTimer(i, round, point, name, time, url) {
 	timerID[i] = setTimeout(
 		function () {
 			map.panTo(point);
+			document.getElementById("info").innerHTML = name + "<br><a href=" + url + " target=blank>(" + url + ")</a>";
+			document.getElementById("img").innerHTML = "<img src=img/" + name + ".jpg height=250 width=100% alt=" + name + ">";
 			document.getElementById("now").innerHTML = "(" + round + "周目)現在、" + name + "です。";
-			console.log("(" + round + "周目)現在、" + name + "です。");// デバッグ用
+			//console.log("(" + round + "周目)現在、" + name + "です。");// デバッグ用
 		},
-		i * 1000
+		i * time
 	);
 }
 
